67afc035f5a8f2792253941d84f67b2a3a2c2a4d
Rajnish Shonkhia <rshonkhia65@gmail.com>
2024-04-18 21:08:47 +0530

    first commit

---
+++ src/main/java/com/eazybytes/cards/cards/dto/Check.java
@@ -0,0 +1,10 @@
+package com.eazybytes.cards.cards.dto;
+
+public class Check {
+    private int id;
+    private String name;
+    public Check(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
